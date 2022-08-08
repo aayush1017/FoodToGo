7dc2677d5a5a7e5e5c56ae3680a83d6962dbc76e
aayush1017 <d.aayushjain@gmail.com>
2022-08-08 15:01:33 +0530

    Styling - Fonts

--- App.js
+++ App.js
@@ -3,10 +3,27 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { RestaurantsScreen } from './src/features/restaurants/screens/rest.screen';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { theme } from './src/infrastructure/theme'
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 const App = () => {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
 
   return (
     <>
